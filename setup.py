46a919791ef7a82c946aa51484bf773976a79380
Adam Strauch <cx@initd.cz>
2011-08-08 17:05:59 +0200

    Initialization commit

---
+++ setup.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python
+
+import os
+from setuptools import setup, find_packages
+
+def read(fname):
+    return open(os.path.join(os.path.dirname(__file__), fname)).read()
+
+setup(
+    name = "InvoiceGenerator",
+    version = "0.1.0",
+    author = "Adam Strauch",
+    author_email = "cx@initd.cz",
+    description = ("Library to generate PDF invoice."),
+    license = "BSD",
+    keywords = "invoice invoices generator",
+    url = "https://github.com/creckx/InvoiceGenerator",
+    packages=find_packages(exclude=['ez_setup', 'examples', 'tests']),
+    long_description="This is library to generate simple PDF invoice. It's based on ReportLab.",
+    classifiers=[
+        "Development Status :: 3 - Alpha",
+        "Topic :: Utilities",
+        "License :: OSI Approved :: BSD License",
+    ],
+    install_requires=[
+        "reportlab"
+        ],
+)
